7a0a98bebb60e1f2dba9f629a464d85ec823d119
LiorB90 <olele123>
2019-03-12 22:43:11 +0200

    Connected BT

--- src/main/java/com/example/roboapp_2/MainActivity.java
+++ src/main/java/com/example/roboapp_2/MainActivity.java
@@ -28,6 +28,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -139,37 +140,15 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        startBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startBtn.setText("Running");
-                startBtn.setTextColor(Color.GREEN);
-                stopBtn.setEnabled(true);
-                char[][] room = new char[6][6];
-                for (int i = 0; i < 6; i++) {
-                    for (int j = 0; j < 6; j++) {
-                        if (i == 0 || i == 5) {
-                            room[i][j] = 'w';
-                        } else {
-                            if (j == 0 || j == 5) {
-                                room[i][j] = 'w';
-                            } else {
-                                room[i][j] = 'k';
-                            }
-                        }
-                    }
-                }
-                for (int i = 0; i < 6; i++)
-                    for (int j = 0; j < 6; j++)
-                        global_room[i][j] = room[i][j];
-                System.out.println("done");
-                showMapBtn.setEnabled(true);
-
-
-                ///// Test //////
-                ///// End Test //////
-            }
-        });
+//        startBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//
+//                ///// Test //////
+//                ///// End Test //////
+//            }
+//        });
 
         stopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -208,51 +187,14 @@ public class MainActivity extends AppCompatActivity {
 
 ///////////////// Test //////////////////////
     public void Start(View v) throws IOException{
+        Log.d(TAG,"what is up manniga");
         startBtn.setText("Running");
         startBtn.setTextColor(Color.GREEN);
         stopBtn.setEnabled(true);
 //        createMAT();
         showMapBtn.setEnabled(true);
-//
-//        if (BTA != null) {
-//            if (BTA.isEnabled()) {
-//                Set<BluetoothDevice> bondedDevices = BTA.getBondedDevices();
-//
-//                if(bondedDevices.size() > 0) {
-//                    Object[] devices = (Object []) bondedDevices.toArray();
-////                            BluetoothDevice device = (BluetoothDevice) devices[position];
-//                    ParcelUuid[] uuid = BTD.getUuids();
-//                    BluetoothSocket socket = BTD.createRfcommSocketToServiceRecord(uuid[0].getUuid());
-//                    socket.connect();
-//                    outputStream = socket.getOutputStream();
-//                    inStream = socket.getInputStream();
-//                }
-//
-//                Log.e("error", "No appropriate paired devices.");
-//            } else {
-//                Log.e("error", "Bluetooth is disabled.");
-//            }
-//        }
-//        write("s");
-//    }
-//
-//    public void write(String s) throws IOException {
-//        outputStream.write(s.getBytes());
-//    }
-//
-//    public void run() {
-//        final int BUFFER_SIZE = 1024;
-//        byte[] buffer = new byte[BUFFER_SIZE];
-//        int bytes = 0;
-//        int b = BUFFER_SIZE;
-//
-//        while (true) {
-//            try {
-//                bytes = inStream.read(buffer, bytes, BUFFER_SIZE - bytes);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
+//        sendBT();
+        sendBTClass();
     }
 ///////////////// End Test //////////////////////
 
@@ -282,9 +224,80 @@ public class MainActivity extends AppCompatActivity {
             for (int j = 0; j < 6; j++)
                 global_room[i][j] = room[i][j];
         System.out.println("done");
+    }
+
+    private void sendBTClass(){
+        try {
+
+            Log.d(TAG, "In SendBTClass");
+            ParcelUuid[] parcels = BTD.getUuids();
+            List<UUID> uuids = new ArrayList<UUID>();
+            for (ParcelUuid parcel : parcels) {
+                uuids.add(parcel.getUuid());
+            }
+            BluetoothConnector BTC = new BluetoothConnector(BTD, false, BTA, uuids);
+            Log.d(TAG, "Created Connector");
+            BluetoothConnector.BluetoothSocketWrapper socket = BTC.connect();
+        }catch(Exception e){
+            Log.d(TAG,e.getMessage());
+        }
 
     }
 
+    private void sendBT(){
+        try {
+            Log.d(TAG,"Start Init");
+            init();
+
+            Log.d(TAG,"Start Writing");
+            write("s");
+
+            Log.d(TAG,"Ass");
+        }catch (Exception e){
+            Log.d(TAG,e.getMessage());
+        }
+    }
+
+    private void init() throws IOException {
+        BluetoothAdapter blueAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (blueAdapter != null) {
+            if (blueAdapter.isEnabled()) {
+                Set<BluetoothDevice> bondedDevices = blueAdapter.getBondedDevices();
+
+                if(bondedDevices.size() > 0) {
+                    //Object[] devices = (Object []) bondedDevices.toArray();
+                    ParcelUuid[] uuid = BTD.getUuids();
+                    BluetoothSocket socket = BTD.createRfcommSocketToServiceRecord(uuid[0].getUuid());
+                    socket.connect();
+                    outputStream = socket.getOutputStream();
+                    inStream = socket.getInputStream();
+                }
+
+                Log.e("error", "No appropriate paired devices.");
+            } else {
+                Log.e("error", "Bluetooth is disabled.");
+            }
+        }
+    }
+
+    public void write(String s) throws IOException {
+        outputStream.write(s.getBytes());
+    }
+
+    public void run() {
+        final int BUFFER_SIZE = 1024;
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bytes = 0;
+        int b = BUFFER_SIZE;
+
+        while (true) {
+            try {
+                bytes = inStream.read(buffer, bytes, BUFFER_SIZE - bytes);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     @Override
     protected void onResume() {
         super.onResume();
